8b8b9dba809b4272a7482d871779e134ef330f87
dattphan15 <ken.phan@live.ca>
2021-07-05 22:39:26 -0700

    render Button instance

--- Random.js
+++ Random.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Button } from './Button.js';
 
 class Random extends React.Component {
   constructor(props) {
@@ -43,6 +44,7 @@ class Random extends React.Component {
         <h1 className={this.isLight() ? 'white' : 'black'}>
           Your color is {this.formatColor(this.state.color)}.
         </h1>
+        <Button light={this.isLight()} />
       </div>
     );
   }
